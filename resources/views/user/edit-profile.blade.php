8a8930c1740236f738901eeebb3309a9560f2060
Sam Buenaventura <94029953+sambuenaventura@users.noreply.github.com>
2024-04-03 18:20:49 +0800

    refactor: Change input type to number for student number field

--- resources/views/user/edit-profile.blade.php
+++ resources/views/user/edit-profile.blade.php
@@ -231,7 +231,7 @@
                                             <label></label>
                                         @else
                                             <!-- Render the input field and label for non-admin users -->
-                                            <input type="text" id="student_number" name="student_number" value="{{ auth()->user()->student_number }}" class="form-control bg-light" placeholder="Student Number" aria-label="Student Number">
+                                            <input type="number" id="student_number" name="student_number" value="{{ auth()->user()->student_number }}" class="form-control bg-light" placeholder="Student Number" aria-label="Student Number">
                                             <label for="student_number">Student Number</label>
                                         @endif
                                         
@@ -295,7 +295,7 @@
                                     </div> --}}
                                     <div class="col">
                                         <div class="form-floating position-relative">
-                                            <input type="password" id="pin" name="pin" value="{{ auth()->user()->pin ?? '' }}" class="form-control bg-light pr-5" placeholder=" " aria-label="PIN">
+                                            <input type="password" id="pin" name="pin" value="{{ auth()->user()->pin ?? '' }}" onkeypress="return event.charCode >= 48 && event.charCode <= 57" class="form-control bg-light pr-5" placeholder=" " aria-label="PIN" maxlength="4">
                                             <label for="pin">PIN</label>
                                             <svg id="eyeIconP" xmlns="http://www.w3.org/2000/svg" height="22" viewBox="0 -960 960 960" width="22" class="mx-2 position-absolute end-0 top-50 translate-middle-y" onclick="togglePinVisibility()" style="cursor: pointer;">
                                                 <path d="m644-428-58-58q9-47-27-88t-93-32l-58-58q17-8 34.5-12t37.5-4q75 0 127.5 52.5T660-500q0 20-4 37.5T644-428Zm128 126-58-56q38-29 67.5-63.5T832-500q-50-101-143.5-160.5T480-720q-29 0-57 4t-55 12l-62-62q41-17 84-25.5t90-8.5q151 0 269 83.5T920-500q-23 59-60.5 109.5T772-302Zm20 246L624-222q-35 11-70.5 16.5T480-200q-151 0-269-83.5T40-500q21-53 53-98.5t73-81.5L56-792l56-56 736 736-56 56ZM222-624q-29 26-53 57t-41 67q50 101 143.5 160.5T480-280q20 0 39-2.5t39-5.5l-36-38q-11 3-21 4.5t-21 1.5q-75 0-127.5-52.5T300-500q0-11 1.5-21t4.5-21l-84-82Zm319 93Zm-151 75Z"/>                                            
@@ -304,6 +304,7 @@
                                         <button type="button" class="btn btn-success btn-submit mt-2" onclick="generatePin()">Generate PIN</button>
                                         <button type="button" class="btn btn-danger btn-submit btn-clear mt-2" onclick="clearPin()">Clear PIN</button>
                                     </div>
+                                    
 
                                     
                                     
